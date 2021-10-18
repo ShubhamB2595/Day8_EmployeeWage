74a41fa782bef4264b7fd90808473616c58ea91f
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-18 17:50:29 +0530

    UC1 employee attendance checking is done

--- src/com/empwage/Employee.java
+++ src/com/empwage/Employee.java
@@ -5,6 +5,12 @@ public class Employee {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to employee wage computation Program");
+		checkAttendance();
 	}
-
+	
+	public static double checkAttendance() {
+		double attendance = Math.floor(Math.random() * 10 % 2);
+		System.out.println("Attendance of the employee is: " + attendance);
+		return attendance;
+	}
 }
