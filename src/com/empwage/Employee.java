4bffbea63dc489f94d0e6bc0b568b186348873a4
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-18 20:17:29 +0530

    UC6 calculated monthly wage using while loop

--- src/com/empwage/Employee.java
+++ src/com/empwage/Employee.java
@@ -7,6 +7,7 @@ public class Employee {
 	// declaring constants
 	static int WAGE_PER_HR = 20;
 	static int MAX_WORK_DAYS = 20;
+	static int MAX_WORK_HRS = 100;
 	static final int FULL_TIME = 1;
 	static final int PART_TIME = 2;
 	
@@ -20,7 +21,9 @@ public class Employee {
 	//method for checking attendance
 	private static int checkAttendance() {
 		int workHrs = 0;
-		for (int i = 1; i<=MAX_WORK_DAYS; i++) {
+		int workDays = 0;
+		while (workHrs <= MAX_WORK_HRS && workDays < MAX_WORK_DAYS) {
+			workDays++;
 			int attendance = (int) Math.floor(Math.random() * 10 % 3);
 		
 			switch(attendance) {
@@ -31,7 +34,7 @@ public class Employee {
 				default:
 					workHrs += 0;
 			}
-			System.out.println("Employee is present for day: " + i + " Working Hrs: " + workHrs);
+			System.out.println("Employee is present for day: " + workDays + " Working Hrs: " + workHrs);
 		}
 		return workHrs;	
 	}
