b171aff5d7385c01eea631733f41b0acf905f433
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-18 19:08:22 +0530

    UC4 solving using switch case statement is done

--- src/com/empwage/Employee.java
+++ src/com/empwage/Employee.java
@@ -6,6 +6,8 @@ public class Employee {
 
 	// declaring constants
 	static int WAGE_PER_HR = 20;
+	static final int FULL_TIME = 1;
+	static final int PART_TIME = 2;
 	
 	//Main Method
 	public static void main(String[] args) {
@@ -18,17 +20,16 @@ public class Employee {
 	private static int checkAttendance() {
 		int workHrs = 0;
 		int attendance = (int) Math.floor(Math.random() * 10 % 3);
-		if (attendance == 1) {
+		switch(attendance) {
+		case FULL_TIME:
 			workHrs = 8;
 			System.out.println("Employee is present for full day");
 			return workHrs;
-		}
-		else if (attendance == 2) {
+		case PART_TIME:
 			workHrs = 4;
 			System.out.println("Employee is present for half day");
 			return workHrs;
-		}
-		else {
+		default:
 			workHrs = 0;
 			System.out.println("Employee is absent");
 			return workHrs;
