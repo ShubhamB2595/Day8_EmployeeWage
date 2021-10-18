b5844e61c5971def9639ea7f93b821bddd541b21
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-18 17:22:24 +0530

    Welcome to employee wage computation Program

---
+++ src/com/empwage/Employee.java
@@ -0,0 +1,10 @@
+package com.empwage;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to employee wage computation Program");
+	}
+
+}
